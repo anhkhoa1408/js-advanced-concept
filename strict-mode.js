c87819eea4d91b1ba86f16b2ed12a0541d640050
anhkhoa1408 <akhoa981@gmail.com>
2024-06-15 23:03:21 +0700

    feat(mode): strict mode

---
+++ strict-mode.js
@@ -0,0 +1,34 @@
+"use strict";
+// fullName = "Khoa";
+// console.log("fullName:", fullName);
+
+// 1. Writable: false
+// const student = {
+//   name: "Khoa",
+// };
+
+// Object.freeze(student);
+
+// Object.defineProperty(student, "name", {
+//   value: "Khoa",
+//   writable: false,
+// });
+
+// student.name = "Na";
+
+// 2. Delete object
+// delete student;
+
+// 3. Duplicate parameter
+// console.log("sum:", sum(3, 10));
+// function sum(a, a) {
+//   return a + a;
+// }
+
+// 4. Declare function in code block, then function can only be used in code block
+// {
+//   function sum(a, b) {
+//     return a + b;
+//   }
+// }
+// console.log(sum(3, 3));
